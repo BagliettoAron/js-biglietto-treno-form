0532a276c3ed9ed54dfc320f5e61a7620f0818dd
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-09 20:01:07 +0200

    Add: script for display output section in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // INPUT
 
-const userName = document.getElementById("user-name").value;
-console.log(userName);
+// mostro la sezione del biglietto con i risultati dei dati di imput
+const formBtn = document.getElementById("form-btn")
+console.log(formBtn)
+
+formBtn.addEventListener("click",
+    function(){
+        const resultContainer = document.querySelector(".ms_output-section")
+        console.log(resultContainer)
+        resultContainer.classList.remove("d-none");
+    }
+);
 
