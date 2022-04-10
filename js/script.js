039305c3c11b267f0d3b56db78d3567f4b0f01a5
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-10 09:57:06 +0200

    WIP: discount script in .js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,52 @@
 // INPUT
 
-// mostro la sezione del biglietto con i risultati dei dati di imput
-const formBtn = document.getElementById("form-btn")
-console.log(formBtn)
 
-formBtn.addEventListener("click",
-    function(){
-        const resultContainer = document.querySelector(".ms_output-section")
-        console.log(resultContainer)
-        resultContainer.classList.remove("d-none");
+
+
+// const ageRange = document.getElementById ("age-range").value
+
+// mostro la sezione del biglietto con i risultati dei dati di imput
+
+const outputSection = document.getElementById("form-btn")
+console.log(outputSection)
+
+outputSection.addEventListener("click",
+function(){
+    // prelevo gli imput
+    const userName = document.getElementById("user-name").value;
+    console.log(userName)
+    
+    const routeDistance = document.getElementById("route-distance").value;
+    console.log(routeDistance)
+
+    const routePrice = (routeDistance) * (0.21)
+    console.log(routePrice)
+    
+    let ageRange = document.getElementById("age-range");
+    console.log(ageRange)
+
+    const under18 = document.getElementById("under18");
+
+    const over65 = document.getElementById("over65");
+
+    let discount = 0;
+    
+     if (ageRange = under18) {
+        discount = routePrice * 0.2;
+    } else if (ageRange = over65) {
+        discount = routePrice * 0.4;
     }
+    console.log(discount)
+        
+
+
+
+    // visualizzo la pagina di output
+    const outputSection = document.querySelector(".ms_output-section")
+    console.log(outputSection)
+    outputSection.classList.remove("d-none");
+    }
+
 );
+
 
